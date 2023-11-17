a56dbcbefd5cc054eddc000752cf3f13f84066b6
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-11-17 09:49:44 +0530

    React Log-In Page

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <div class="footer bg-dark text-center text-light">
+        <p>Copyright All Rights Reserverd.</p>
+      </div>
+    </div>
+  );
+}
